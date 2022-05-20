1483bd7d5c6f017aabfa9dc854fba516a9231f2d
maikgr <maikgibranr@gmail.com>
2022-05-20 12:46:21 +0800

    Fix infinite loop in getting random spawn point

--- Assets/Scripts/Entities/ResourceSpawnArea.cs
+++ Assets/Scripts/Entities/ResourceSpawnArea.cs
@@ -13,18 +13,20 @@ namespace Permanence.Scripts.Entities
 
     public static class ResourceSpawnAreaExtension
     {
-        private static Vector2 GetRandomSpawnPoint(this ResourceSpawnArea spawnArea)
+        public static Vector2 GetRandomSpawnPoint(this ResourceSpawnArea spawnArea, Vector2 sourcePos)
         {
             var hasObject = true;
             Vector2 randomPos = Vector2.zero;
-            while (hasObject)
+            var maxTries = 50; // In case there's no suitable spawn point found
+            while (hasObject || maxTries > 0)
             {
                 randomPos = new Vector2(
-                    UnityEngine.Random.Range(spawnArea.MinLocalPoint.x, spawnArea.MaxLocalPoint.x),
-                    UnityEngine.Random.Range(spawnArea.MinLocalPoint.y, spawnArea.MaxLocalPoint.y)
+                    UnityEngine.Random.Range(sourcePos.x + spawnArea.MinLocalPoint.x, sourcePos.x + spawnArea.MaxLocalPoint.x),
+                    UnityEngine.Random.Range(sourcePos.y + spawnArea.MinLocalPoint.y, sourcePos.y + spawnArea.MaxLocalPoint.y)
                 );
                 var hitInfo = Physics2D.Raycast(randomPos, Vector2.zero);
                 hasObject = hitInfo.collider != null;
+                --maxTries;
             }
             return randomPos;
         }
